5100bba180a8174e774e47a4850a5a5d12768b37
ohansck <ohansck@gmail.com>
2022-10-05 09:52:39 +0100

    update

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '5fey8sl955';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-o1jz1bhh.us.auth0.com',
+  clientId: 'nL8bliXgxlvYS8TcyUVwdZ2CmrqExUSB',
+  callbackUrl: 'http://localhost:3000/callback'
+};
